0447fd8bf2bc5d455cd36af3a6e6bb440e6a4e7a
Sdamu <shr1994@yeah.net>
2018-05-23 16:06:29 +0800

    生成和正样本大小相同的负样本对的 txt 文件

--- data_txt.py
+++ data_txt.py
@@ -1,33 +1,34 @@
 import os
+import random
 from itertools import combinations
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASEPATH, 'per')
+DATA_PATH = os.path.join(BASEPATH, 'AugDataSet')
 # 5 个大的分类
 class_names = os.listdir(DATA_PATH)
 
-# 产生正样本对
-ff = open("positive_pairs_path.txt",'wb')
-for class_name in class_names:
-    # 每一大类下的 5 个细分类
-    concrete_class_path = os.path.join(DATA_PATH,class_name)
-    concrete_class_names = os.listdir(concrete_class_path)
-    for concrete_class_name in concrete_class_names:
-        concrete_file_path = os.path.join(concrete_class_path,concrete_class_name)
-        concrete_file_names = os.listdir(concrete_file_path)
-        # 生成从 20 个文件中随机选出两个的全部情况，即 C(20)2
-        combins = [c for c in combinations(range(20), 2)]  # 生成 C(20)2 中情况
-        for combin in combins:
-            (pair_a, pair_b) = combin
-            #print(len(concrete_file_names),pair_a,pair_b)
-            first = os.path.join(concrete_file_path,concrete_file_names[pair_a])
-            second = os.path.join(concrete_file_path,concrete_file_names[pair_b])
-            lines = first + ' '+second+ '\n'
-            line = lines.encode(encoding="utf-8")
-            # print(line)
-            ff.write(line)
-
-ff.close()
+# # 产生正样本对
+# ff = open("positive_pairs_path.txt",'wb')
+# for class_name in class_names:
+#     # 每一大类下的 5 个细分类
+#     concrete_class_path = os.path.join(DATA_PATH,class_name)
+#     concrete_class_names = os.listdir(concrete_class_path)
+#     for concrete_class_name in concrete_class_names:
+#         concrete_file_path = os.path.join(concrete_class_path,concrete_class_name)
+#         concrete_file_names = os.listdir(concrete_file_path)
+#         # 生成从 200 个文件中随机选出两个的全部情况，即 C(20)2
+#         combins = [c for c in combinations(range(200), 2)]  # 生成 C(20)2 中情况
+#         for combin in combins:
+#             (pair_a, pair_b) = combin
+#             #print(len(concrete_file_names),pair_a,pair_b)
+#             first = os.path.join(concrete_file_path,concrete_file_names[pair_a])
+#             second = os.path.join(concrete_file_path,concrete_file_names[pair_b])
+#             lines = first + ' '+ second+ '\n'
+#             line = lines.encode(encoding="utf-8")
+#             # print(line)
+#             ff.write(line)
+#
+# ff.close()
 
 # 生成负样本对
 file = open("negative_pairs_path.txt",'wb')
@@ -63,10 +64,18 @@ for i in range(len(objectCombinations)):
             test.append([object_path_path1[m], object_path_path2[n]])
 
 
-for i in range(len(test)):
-    lines = test[i][0] + ' ' + test[i][1] + '\n'
-    line = lines.encode(encoding="utf-8")
-    file.write(line)
+# 在 [0,49000000) 之间随机生成 995000 个随机数，
+# 讲这些随机数所对应的行写入 txt 文件
+
+b_list = range(0,49000000)
+choose_Id = random.sample(b_list, 995000)
+
+for i in range(len(choose_Id)):
+        # 如果 i 在当前选中的索引中，进行写入
+        lines = test[choose_Id[i]][0] + ' ' + test[choose_Id[i]][1] + '\n'
+        line = lines.encode(encoding="utf-8")
+        file.write(line)
+        print(choose_Id[i])
 
 file.close()
 
