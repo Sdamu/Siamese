b6486d82b4768fda0d5bfe465ea07e2b45983380
Sdamu <shr1994@yeah.net>
2018-05-25 15:13:53 +0800

    modify maxpool function for name

--- model.py
+++ model.py
@@ -8,20 +8,21 @@ class SIAMESE(object):
                 net = slim.layers.conv2d(input, 32, [7, 7], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
 
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net,[1, 2, 2, 1],strides=[1,2,2,1],padding='SAME',name="conv_1")
             with tf.variable_scope("conv2") as scope:
                 net = slim.layers.conv2d(net, 64, [5, 5], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
-
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net,[1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name="conv_2")
             with tf.variable_scope("conv3") as scope:
                 net = slim.layers.conv2d(net, 128, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
-
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net, [1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name="conv_3")
             # with tf.variable_scope("conv4") as scope:
             #     net = slim.layers.conv2d(net, 256, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
             #                                    weights_initializer=slim.xavier_initializer_conv2d(),
@@ -31,9 +32,9 @@ class SIAMESE(object):
                 net = slim.layers.conv2d(net, 256, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net, [1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name="conv_4")
                 output_0 = slim.layers.flatten(net)
-
             # with tf.variable_scope("conv6") as scope:
             #     net = slim.layers.conv2d(net, 512, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
             #                                    weights_initializer=slim.xavier_initializer_conv2d(),
@@ -43,24 +44,25 @@ class SIAMESE(object):
                 net = slim.layers.conv2d(net, 512, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net, [1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name="conv_5")
                 output_1 = slim.flatten(net)
 
             with tf.variable_scope("conv6") as scope:
                 net = slim.layers.conv2d(net, 32, [3, 3], activation_fn=None, padding='SAME',
                                                weights_initializer=slim.xavier_initializer_conv2d(),
                                                scope=scope, reuse=reuse)
-                net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
-
+                # net = slim.layers.max_pool2d(net, [2, 2], padding='SAME')
+                net = tf.nn.max_pool(net, [1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name="conv_6")
             output_2 = slim.layers.flatten(net)
 
-            net = tf.concat([output_0, output_1, output_2], 1)
+            net = tf.concat([output_0, output_1, output_2], 1,name='concat')
 
             # add hidden layer1
             hidden_Weights = tf.Variable(tf.truncated_normal([11136, 2048], stddev=0.1))
             hidden_biases = tf.Variable(tf.constant(0.1, shape=[2048]))
             net = slim.dropout(net,is_training=True, keep_prob=0.5)
-            net = tf.nn.relu(tf.matmul(net, hidden_Weights) + hidden_biases)
+            net = tf.nn.relu(tf.matmul(net, hidden_Weights) + hidden_biases,name="hidden_layer1")
 
             with tf.variable_scope("dropout") as scope:
                 net = slim.layers.dropout(net, keep_prob=0.7, scope=scope)
@@ -69,7 +71,7 @@ class SIAMESE(object):
             hidden_Weights = tf.Variable(tf.truncated_normal([2048, 128], stddev=0.1))
             hidden_biases = tf.Variable(tf.constant(0.1, shape=[128]))
             net = slim.dropout(net, is_training=True, keep_prob=0.5)
-            net = tf.nn.relu(tf.matmul(net, hidden_Weights) + hidden_biases)
+            net = tf.nn.relu(tf.matmul(net, hidden_Weights) + hidden_biases,name="final_out")
 
         return net
 
@@ -88,7 +90,7 @@ class SIAMESE(object):
 
         # Calculate Accuracy
         with tf.name_scope("accuracy"):
-            accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.round(y_), y), tf.float32))
+            accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.round(y_), y), tf.float32),name="accuracy")
 
         return model1, model2, y_, loss, accuracy
 
