048cadce5df4d8104600180f051f5b71ec9875b8
Sdamu <shr1994@yeah.net>
2018-05-29 16:53:02 +0800

    modify pydown

--- py_up_and_down.py
+++ py_up_and_down.py
@@ -2,11 +2,12 @@
 import cv2
 
 srcImage = cv2.imread('bottle1.jpg')
-
+srcImage1 = srcImage
 print(srcImage.shape[0],srcImage.shape[1])
 ratio = 1
 for i in range(3):
-    ratio *= 2
-    srcImage = cv2.pyrDown(srcImage,(srcImage.shape[1]/ratio,srcImage.shape[0]/ratio))
+    srcImage = cv2.pyrDown(srcImage)
     ImageName = "py_"+str(i+1)+".jpg"
     cv2.imwrite(ImageName,srcImage)
+
+cv2.imwrite("test.jpg",srcImage1)
